1dc66225c596a4e3a3e51af5876b325a1874854f
beckyweeks14 <mauransnow@Weeks-MacBook-Pro.local>
2022-12-13 16:02:22 -0500

    setup edits

---
+++ routes/skills.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* Get skills listing*/
+router.get("/", function(req, res) {
+const skillsRouter = require("./routes/skills");
+})
+
+module.exports = router;
